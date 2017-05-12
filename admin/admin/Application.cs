c35f5854f008cd91324b441c7b797ca46293230e
Christoph Stiller <c.stiller@live.de>
2017-05-12 20:10:36 +1000

    Commit to GitHub

---
+++ admin/admin/Application.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin
+{
+  public static class Application
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length == 0)
+      {
+        Console.WriteLine("Usage:\nadmin executable.exe arg0 arg1 arg2 ...");
+        return;
+      }
+
+      string path = args[0].Trim('\"');
+      string strargs = "";
+      int num = 0;
+
+      args.ToList().ForEach(arg =>
+      {
+        if (num++ > 0)
+          strargs += arg + " ";
+      });
+
+      strargs = strargs.TrimEnd(' ');
+
+      ElevateRightsWindows.RunElevated(path, strargs);
+    }
+  }
+}
